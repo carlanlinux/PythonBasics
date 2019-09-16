697fa77787ef4317a3e7a249fc1c83d5a935cb04
carlosdelucas <carlos.lucas6@educa.madrid.org>
2019-09-16 15:17:00 +0200

    Hasta tema 6

---
+++ 7_Iteration/7.9_NamingVariables.py
@@ -0,0 +1,9 @@
+'''
+Use singular nouns
+for the iterator variable (things like “song”, “book”, “post”, “letter”, “word”).
+Use plural nouns
+for the sequence variable (things like “songs”, “books”, “posts”, “letters”, “words”).
+'''
+
+for genre in genres:
+    print(genre)
--- 7_Iteration/7_8_ImageProcessing.py
+++ 7_Iteration/7_8_ImageProcessing.py
@@ -1,8 +1,21 @@
 import image
 
-p = image.Pixel(45, 76, 200)
-print(p.getRed())
-p.setRed(66)
-print(p.getRed())
-p.setBlue(p.getGreen())
-print(p.getGreen(), p.getBlue())
+img = image.Image("Luther.jpg")
+win = image.ImageWin(img.getWidth(), img.getHeight())
+img.draw(win)
+img.setDelay(1,15)   # setDelay(0) turns off animation
+
+for row in range(img.getHeight()):
+    for col in range(img.getWidth()):
+        p = img.getPixel(col, row)
+
+        newred = 255 - p.getRed()
+        newgreen = 255 - p.getGreen()
+        newblue = 255 - p.getBlue()
+
+        newpixel = image.Pixel(newred, newgreen, newblue)
+
+        img.setPixel(col, row, newpixel)
+
+img.draw(win)
+win.exitonclick()
